20c4a5311fbb2e0fda48f46b03f1e8d4a46000b3
scaillet <scaillet@sophiagenetics.com>
2019-05-26 15:50:22 +0200

    Final solution 100%

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,12 +1,6 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -20,13 +14,11 @@ class Solution {
         Problem problem = new Problem();
 
         long t0 = System.currentTimeMillis();
-        problem.morse = in.next();
-        int nbWords = in.nextInt();
-        for (int i = 0; i < nbWords; i++) {
-            String word = in.next();
-            problem.addEntry(word.toUpperCase());
+        int nbSequences = in.nextInt();
+        for (int i = 0; i < nbSequences; i++) {
+            String sequence = in.next();
+            problem.addSequence(sequence.toUpperCase());
         }
-        problem.pack();
         long t1 = System.currentTimeMillis();
         System.err.println("Init in " + (t1 - t0) + "ms");
 
@@ -34,159 +26,141 @@ class Solution {
         // To debug: System.err.println("Debug messages...");
 
         ProblemSolver solver = new ProblemSolver(problem);
-        long result = solver.solve();
+        int result = solver.solve();
         System.out.println(result);
     }
 }
 
 class Problem {
-    String morse;
-    Set<String> dictionary = new TreeSet<>();
-    Set<String> dictionaryFirstLetters = new TreeSet<>();
-    Map<String, List<String>> wordsInMorseByFirstLetter = new HashMap<>();
+    List<String> sequences = new ArrayList<>();
 
-    void addEntry(String word) {
-        dictionary.add(word);
-    }
-
-    void loadDictionary(String rawDictionary) {
-        dictionary.addAll(Arrays.asList(rawDictionary.split("\\n")));
-    }
-
-    void pack() {
-        dictionaryFirstLetters = dictionary.stream()
-                .map(word -> word.substring(0, 1))
-                .collect(Collectors.toSet());
-
-        for (String word : dictionary) {
-            String firstLetter = word.substring(0, 1);
-            wordsInMorseByFirstLetter.computeIfAbsent(firstLetter, (letter) -> new ArrayList<>()).add(Morse.buildMorseSequence(word));
-        }
+    void addSequence(String word) {
+        sequences.add(word);
     }
 }
 
 class ProblemSolver {
     Problem problem;
-    Map<String, Long> sequencesToCountMap = new HashMap<>();
 
     ProblemSolver(Problem problem) {
         this.problem = problem;
-        this.sequencesToCountMap.put(problem.morse, 0L); // initial value
     }
 
-    long solve() {
-        // Max duration = 1.8s
-
-        boolean changed = findFirstWord();
-        while (changed) {
-            changed = findFirstWord();
+    int solve() {
+        // loop through all combinations of all sequences
+        int arr[] = new int[problem.sequences.size()];
+        for (int i = 0; i < problem.sequences.size(); i++) {
+            arr[i] = i;
         }
 
-        return sequencesToCountMap.entrySet().stream()
-                .filter(e -> e.getKey().equals(""))
-                .mapToLong(Map.Entry::getValue)
-                .sum();
+        Combination combination = new Combination();
+        combination.permuteHelper(arr, 0);
+        List<int[]> indexCombinations = combination.combinations;
+
+        int min = Integer.MAX_VALUE;
+        for (int[] indexCombi : indexCombinations) {
+            int length = mergeCombination(indexCombi);
+            min = Math.min(min, length);
+        }
+
+        return min;
     }
 
-    private boolean findFirstWord() {
-        Map<String, Long> sequenceMap = new HashMap<>();
-        boolean hasFoundSomething = false;
+    private String combiToString(int[] indexCombi) {
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < indexCombi.length; i++) {
+            buf.append(problem.sequences.get(indexCombi[i])).append("-");
+        }
+        return buf.toString();
+    }
 
-        for (String sequence : sequencesToCountMap.keySet()) {
-            if (!sequence.isEmpty()) {
-                Map<String, Long> sequenceMapForThisWord = findFirstWord(sequence, sequencesToCountMap.get(sequence));  // remove the entry --> will vbe replaced by sub-entries
-                if (!sequenceMapForThisWord.isEmpty()) {
-                    hasFoundSomething = true;
+    private int mergeCombination(int[] indexCombi) {
+        StringBuilder buf = new StringBuilder(problem.sequences.get(indexCombi[0]));
+        for (int i = 1; i < indexCombi.length; i++) {
+            String mergedString = mergeStrings(buf.toString(), problem.sequences.get(indexCombi[i]));
+            buf.append(mergedString);
+        }
+        System.err.println(combiToString(indexCombi) + " becomes " + buf + " of length " + buf.length());
+        return buf.length();
+    }
 
-                    for (String key : sequenceMapForThisWord.keySet()) {
-                        long nbCombi = sequenceMap.getOrDefault(key, 0L);
-                        sequenceMap.put(key, nbCombi + sequenceMapForThisWord.get(key));
-                    }
+    /**
+     * Remove all characters at the end of s1 matching starting chars of s2.
+     * ARRRYU merged to YUGTTT --> GTTT
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public static String mergeStrings(String s1, String s2) {
+        char[] char1s = s1.toCharArray();
+        char[] char2s = s2.toCharArray();
+        boolean matching = false;
+        int indexToCheck = 0;
+
+        if (s1.contains(s2)) {
+            return "";
+        }
+
+        for (int i = 0; i < char1s.length; i++) {
+            if (indexToCheck < char2s.length && char1s[i] == char2s[indexToCheck]) {
+                if (!matching) { // starting of matching sequence
+                    matching = true;
                 }
+                indexToCheck++;
             } else {
-                sequenceMap.put(sequence, sequencesToCountMap.get(sequence));
-            }
-        }
+                matching = false; // finally, the candidate sequence doesn't match
+                indexToCheck = 0;
 
-        sequencesToCountMap = sequenceMap;
-
-        return hasFoundSomething;
-    }
-
-    private Map<String, Long> findFirstWord(String sequence, long nbCombi) {
-        Map<String, Long> localMap = new HashMap<>();
-
-        // check all first letters of the dictionary
-        for (String firstLetter : problem.dictionaryFirstLetters) {
-            if (Morse.startByLetter(sequence, firstLetter)) {
-                // get all words starting by this letter
-                List<String> wordsInMorse = problem.wordsInMorseByFirstLetter.get(firstLetter);
-                for (String wordInMorse : wordsInMorse) {
-                    // if the sequence starts by the word, it makes a new combination
-                    String remainingSequence = Morse.startByWord(sequence, wordInMorse);
-                    if (remainingSequence != null) {
-                        Long nbOccurences = localMap.getOrDefault(remainingSequence, 0L) + 1;
-                        localMap.put(remainingSequence, nbOccurences);
-                    }
+                if (indexToCheck < char2s.length && char1s[i] == char2s[indexToCheck]) {
+                    matching = true;
+                    indexToCheck++;
                 }
             }
         }
 
-        long multiplicator = nbCombi != 0 ? nbCombi : 1;
-        return localMap.entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue() * multiplicator));
+        StringBuilder buf = new StringBuilder();
+        if (matching) {
+            for (int i = indexToCheck; i < char2s.length; i++) {
+                buf.append(char2s[i]);
+            }
+        } else {
+            buf.append(s2);
+        }
+
+        return buf.toString();
     }
 }
 
-class Morse {
-    private static Map<String, String> MORSE_DICTIONARY = createMorseDictionary();
+class Combination {
 
-    public static boolean startByLetter(String sequence, String firstLetter) {
-        String morseSequence = MORSE_DICTIONARY.get(firstLetter);
-        return sequence.length() >= morseSequence.length() && sequence.startsWith(morseSequence);
-    }
+    List<int[]> combinations = new ArrayList<>();
 
-    public static String startByWord(String sequence, String morseSequence) {
-        int morseSequenceLength = morseSequence.length();
-        if (sequence.length() >= morseSequenceLength && sequence.startsWith(morseSequence)) {
-            return sequence.substring(morseSequenceLength);
+    void permuteHelper(int[] arr, int index) {
+        if (index >= arr.length - 1) { //If we are at the last element - nothing left to permute
+            registerCombination(arr);
+            return;
         }
 
-        return null;
+        for (int i = index; i < arr.length; i++) { //For each index in the sub array arr[index...end]
+            //Swap the elements at indices index and i
+            int t = arr[index];
+            arr[index] = arr[i];
+            arr[i] = t;
+
+            //Recurse on the sub array arr[index+1...end]
+            permuteHelper(arr, index + 1);
+
+            //Swap the elements back
+            t = arr[index];
+            arr[index] = arr[i];
+            arr[i] = t;
+        }
     }
 
-    public static String buildMorseSequence(String word) {
-        return word.chars().mapToObj(c -> MORSE_DICTIONARY.get(String.valueOf((char) c))).collect(Collectors.joining());
-    }
-
-    private static Map<String, String> createMorseDictionary() {
-        Map<String, String> morse = new HashMap<>();
-        morse.put("A", ".-");
-        morse.put("B", "-...");
-        morse.put("C", "-.-.");
-        morse.put("D", "-..");
-        morse.put("E", ".");
-        morse.put("F", "..-.");
-        morse.put("G", "--.");
-        morse.put("H", "....");
-        morse.put("I", "..");
-        morse.put("J", ".---");
-        morse.put("K", "-.-");
-        morse.put("L", ".-..");
-        morse.put("M", "--");
-        morse.put("N", "-.");
-        morse.put("O", "---");
-        morse.put("P", ".--.");
-        morse.put("Q", "--.-");
-        morse.put("R", ".-.");
-        morse.put("S", "...");
-        morse.put("T", "-");
-        morse.put("U", "..-");
-        morse.put("V", "...-");
-        morse.put("W", ".--");
-        morse.put("X", "-..-");
-        morse.put("Y", "-.--");
-        morse.put("Z", "--..");
-        return morse;
+    private void registerCombination(int[] src) {
+        int[] dest = new int[src.length];
+        System.arraycopy(src, 0, dest, 0, src.length);
+        combinations.add(dest);
     }
 }
